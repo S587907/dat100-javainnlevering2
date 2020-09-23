b8e5d89de18f9a28a00ff3d53f3f15e5f9b6b4d1
atle1 <atle1@DESKTOP-1GM91S4.home>
2020-09-23 11:39:57 +0200

    V1

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -5,26 +5,73 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
+		for (int i = 0; i < tabell.length; i++)
+			System.out.println(tabell[i]);
+
 		// TODO
 		throw new UnsupportedOperationException("skrivUt ikke implementert");
 
 	}
 
+	
+	
+	
+	
 	// b)
 	public static String tilStreng(int[] tabell) {
-
+		String stri = "";
+		
+		for (int i = 0; i < tabell.length; i++) {
+			if (i != tabell.length) ;
+		
+			}
+		
 		// TODO
 		throw new UnsupportedOperationException("tilStreng ikke implementert");
 		
+		
 	}
 
+	
+	
+	
+	
+	
+	
+	
 	// c)
 	public static int summer(int[] tabell) {
+		int j = 0;
+		int sum = 0;
+		int sum1 = 0;
+		
+		{
+		for (int i = 0; i < tabell.length; i++) 
+				sum += tabell[i];
+				System.out.println("Sum tabbel: " + sum);
+		}
+		
+		
+		while (j < tabell.length)
+		{
+			sum1 += tabell[j];
+			System.out.print(sum1);
+			j++;
+		}
 
 		// TODO
 		throw new UnsupportedOperationException("summer ikke implementert");
+		
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
@@ -32,6 +79,16 @@ public class Tabeller {
 		throw new UnsupportedOperationException("finnesTall ikke implementert");
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
